d4667fb8cf1982810a4c744a75eb09afb33a4417
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2025-05-11 10:21:23 -0500

    dda

--- vaint/src/bin/lab_03_dda.rs
+++ vaint/src/bin/lab_03_dda.rs
@@ -3,13 +3,18 @@ use glium::winit::event_loop::ActiveEventLoop;
 use glium::{implement_vertex, program};
 use glutin::surface::WindowSurface;
 
+const W: f32 = 1000_f32;
+const H: f32 = 600_f32;
 /// Punto de entrada de la aplicación
 fn main() {
     // 1. The **winit::EventLoop** for handling events.
     let event_loop = glium::winit::event_loop::EventLoop::builder().build().unwrap();
 
     // 2. Create a glutin context and glium Display
-    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new().with_inner_size(1000, 1000).build(&event_loop);
+    let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_inner_size(W as u32, H as u32)
+        .with_title("Lab3 Digital Differential Analyzer")
+        .build(&event_loop);
 
     #[allow(deprecated)]
     event_loop
@@ -34,7 +39,15 @@ pub struct Vertex {
     color: [f32; 3],
 }
 implement_vertex!(Vertex, position, color);
+impl Vertex {
+    pub fn new(position: [f32; 2], color: [f32; 3]) -> Self { Vertex { position, color } }
 
+    pub fn new_red(position: [f32; 2]) -> Self { Vertex::new(position, [1.0, 0.0, 0.0]) }
+
+    pub fn new_green(position: [f32; 2]) -> Self { Vertex::new(position, [0.0, 1.0, 0.0]) }
+
+    pub fn new_blue(position: [f32; 2]) -> Self { Vertex::new(position, [0.0, 0.0, 1.0]) }
+}
 /// Representa un Vector de 2 dimensiones
 pub type Vec2 = [f32; 2];
 
@@ -75,44 +88,11 @@ pub fn programa(display: &glium::Display<WindowSurface>) -> glium::Program {
 }
 
 /// Traduce la posición de coordenadas absolutas a coordenadas de OpenGl -1.0..=1.0.
-pub fn transformar_coordenadas<const N: usize>(coordenadas: (u32, u32)) -> [f32; 2] {
+pub fn transformar_coordenadas(coordenadas: (i32, i32)) -> [f32; 2] {
     let (x, y) = coordenadas;
-    let x = x as f32 / N as f32 * 2.0 - 1.0;
-    let y = y as f32 / N as f32 * -2.0 + 1.0; // Invertir el eje Y
+    let x = x as f32 / W * 2.0 - 1.0;
+    let y = y as f32 / H * -2.0 + 1.0; // Invertir el eje Y
     [x, y]
-}
-
-/// Macro para definir un set de [`Vertex`]
-macro_rules! crear_figura {
-    // Capture: vec, color, first point, then one or more additional points
-    ($vec:expr, $display:expr, $color:expr,
-     ($x0:expr, $y0:expr)
-     $(, ($x:expr, $y:expr))+
-     $(,)?
-    ) => {{
-        use self::transformar_coordenadas;
-        let pos = |x: u32, y: u32| transformar_coordenadas::<1000>((x, y));
-
-        // Build a fixed-size array of points, repeating the first at the end:
-        let shape = [
-            // pos(10, 10),
-            pos($x0, $y0),
-            $(
-                pos($x, $y),
-            )+
-            pos($x0, $y0),
-        ];
-        use self::colorear_coordenadas as colorear;
-        $vec.clear();
-        $vec.extend(colorear(&shape, $color));
-        let result : glium::VertexBuffer<crate::Vertex> = glium::VertexBuffer::new($display, &$vec).unwrap();
-        let index_buffer: glium::IndexBuffer<u16> = {
-            let indices: Vec<u16> = (0..$vec.len() as u16).collect();
-            glium::IndexBuffer::new($display, glium::index::PrimitiveType::LineStrip, &indices).unwrap()
-        };
-        (result, index_buffer)
-
-    }};
 }
 
 /// Iterador que devuelve objetos tipo [`Vertex`] que es usado para ubicar puntos en el canvas
@@ -120,27 +100,57 @@ pub fn colorear_coordenadas(coordenadas: &[[f32; 2]], color: [f32; 3]) -> impl I
     coordenadas.iter().map(move |&pos| Vertex { position: pos, color })
 }
 
+/// Algoritmo DDA (Digital Differential Analyzer) para dibujar líneas en un espacio 2D usando puntos
+/// discretos.
+pub fn dda((x_0, y_0): (i32, i32), (x, y): (i32, i32)) -> Vec<[i32; 2]> {
+    let delta_x = x - x_0;
+    let delta_y = y - y_0;
+
+    let steps = i32::max(delta_x.abs(), delta_y.abs());
+
+    let dx = (delta_x as f32) / steps as f32;
+    let dy = (delta_y as f32) / steps as f32;
+
+    let steps = steps + 1; // Se agrega un paso adicional para incluir el último punto
+    let mut puntos = Vec::with_capacity(steps as usize);
+    for k in 0..steps {
+        let k: f32 = k as f32;
+        let x = x_0 + (dx * k) as i32; // Type-Casting a entero trunca los decimales.
+        let y = y_0 + (dy * k) as i32;
+        puntos.push([x, y]);
+    }
+    debug_assert_eq!(
+        puntos.last().unwrap(),
+        &[x, y],
+        "Se esperaba que el último punto fuera ({}, {}), pero se obtuvo {:?}",
+        x,
+        y,
+        puntos.last().unwrap()
+    );
+    puntos
+}
 /// Contiene la logica sobre el manejo de los eventos a la ventana y la aplicacion durante ejecución
 mod ventana {
+    use glium::index::{NoIndices, PrimitiveType};
     use glium::winit::event::{ElementState, KeyEvent, WindowEvent};
     use glium::winit::event_loop::ActiveEventLoop;
     use glium::winit::keyboard::NamedKey;
     use glium::winit::window::WindowId;
-    use glium::{Surface, uniform};
+    use glium::{Surface, VertexBuffer, uniform};
     use glutin::surface::WindowSurface;
 
-    use super::{colorear_coordenadas, transformar_coordenadas};
+    use crate::{Vertex, dda, transformar_coordenadas};
 
-    /// Codigo para controlar lo que se dibuja en la ventana
-    fn draw_frame(display: &glium::Display<glutin::surface::WindowSurface>) {
-        // Obtener el objeto dibujable a la pantalla
-        let mut target: glium::Frame = display.draw();
-
-        // Limpiar la pantalla
-        target.clear_color(0.0, 0.0, 0.0, 1.0); // Blanco
-
-        // # Inicializacion
-        // For this example a simple identity matrix suffices
+    /// Funcion Auxiliar que dibuja una línea entre dos puntos y aparte dibuja una serie de puntos
+    /// paralela a la línea desplazado según `points_shift`
+    fn dibujar_linea(
+        display: &glium::Display<WindowSurface>,
+        target: &mut glium::Frame,
+        p0: (i32, i32),
+        p: (i32, i32),
+        points_shift: (i32, i32), // Desplazamiento de la línea puntual
+        programa: &glium::Program,
+    ) {
         let uniforms = uniform! {
             matrix: [
                 [1.0, 0.0, 0.0, 0.0],
@@ -149,101 +159,46 @@ mod ventana {
                 [0.0, 0.0, 0.0, 1.0f32]
             ]
         };
+        let draw_params = glium::DrawParameters { ..Default::default() };
+        let vec_shift = |(vec_x, vec_y), pos: [i32; 2]| (pos[0] + vec_x, pos[1] + vec_y);
+
+        // Puntos individuales de la línea
+        let puntos = dda(p0, p) // Realizamos el algoritmo DDA
+            .into_iter()
+            .map(|v| vec_shift(points_shift, v)) // Desplazamos la serie de puntos
+            .map(transformar_coordenadas) // Normalizamos al espacio de coordenadas de OpenGL
+            .map(Vertex::new_blue) // Coloreamos los puntos
+            .collect::<Vec<_>>(); // Recogemos los puntos a un vector
+        let puntos = VertexBuffer::new(display, &puntos).unwrap();
+        target.draw(&puntos, NoIndices(PrimitiveType::Points), programa, &uniforms, &draw_params).ok();
+        let vertexes: [Vertex; 2] = [Vertex::new_red(transformar_coordenadas(p0)), Vertex::new_red(transformar_coordenadas(p))];
+        target // Dibujar la linea con la implementación de OpenGL
+            .draw(
+                &VertexBuffer::new(display, &vertexes).unwrap(),
+                NoIndices(PrimitiveType::LineStrip),
+                programa,
+                &uniforms,
+                &draw_params,
+            )
+            .ok();
+    }
+
+    /// Codigo para controlar lo que se dibuja en la ventana
+    fn draw_frame(display: &glium::Display<WindowSurface>) {
+        // Obtener el objeto dibujable a la pantalla
+        let mut target: glium::Frame = display.draw();
+
+        // Limpiar la pantalla
+        target.clear_color(0.2, 0.7, 0.2, 1.0); // Blanco
+
         let programa = crate::programa(display);
-        let mut figura_vertices = Vec::new();
 
-        // Figura 1 - Crear un rectangulo vertical verde
-        let (rect, index_buffer) =
-            crear_figura!(&mut figura_vertices, display, [0.0, 1.0, 0.0], (33, 60), (233, 60), (233, 180), (33, 180));
-        target.draw(&rect, &index_buffer, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 2 - Rectángulo acostado Rojo
-        let (fig, idxb) =
-            crear_figura!(&mut figura_vertices, display, [1.0, 0.0, 0.0], (260, 30), (380, 30), (380, 210), (260, 210));
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 3 - Rombo
-        let (fig, idxb) =
-            crear_figura!(&mut figura_vertices, display, [0.0, 0.0, 1.0], (496, 33), (583, 120), (496, 208), (408, 120),);
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 4 - Trapecio
-        let (fig, idxb) =
-            crear_figura!(&mut figura_vertices, display, [1.0, 1.0, 0.0], (610, 30), (790, 30), (746, 210), (656, 210));
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 5 - Triángulo
-        let (fig, idxb) = crear_figura!(&mut figura_vertices, display, [1.0, 0.5, 0.0], (818, 195), (893, 45), (968, 195));
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 6 - Triangulo Rectangulo
-        let (fig, idxb) = crear_figura!(&mut figura_vertices, display, [0.0, 1.0, 1.0], (33, 341), (183, 490), (33, 490));
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 7 - Hexágono
-        let (fig, idxb) = crear_figura!(
-            &mut figura_vertices,
-            display,
-            [1.0, 0.0, 1.0],
-            (213, 416),
-            (255, 342),
-            (339, 342),
-            (382, 416),
-            (339, 489),
-            (255, 489)
-        );
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 8 - Pentágono
-        let (fig, idxb) = crear_figura!(
-            &mut figura_vertices,
-            display,
-            [0.0, 0.5, 0.0],
-            (411, 398),
-            (492, 339),
-            (572, 398),
-            (541, 492),
-            (442, 492),
-        );
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 9 - Poligono +
-        let (fig, idxb) = crear_figura!(
-            &mut figura_vertices,
-            display,
-            [0.5, 0.8, 1.0],
-            (602, 356),
-            (642, 356),
-            (642, 316),
-            (762, 316),
-            (762, 356),
-            (802, 356),
-            (802, 476),
-            (762, 476),
-            (762, 516),
-            (642, 516),
-            (642, 476),
-            (602, 476),
-        );
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
-
-        // Figura 10 - Extrella
-        let (fig, idxb) = crear_figura!(
-            &mut figura_vertices,
-            display,
-            [0.8, 0.8, 0.8],
-            (831, 398),
-            (892, 398),
-            (912, 339),
-            (930, 398),
-            (992, 398),
-            (942, 434),
-            (961, 492),
-            (912, 456),
-            (862, 492),
-            (881, 434),
-        );
-        target.draw(&fig, &idxb, &programa, &uniforms, &Default::default()).unwrap();
+        // Lineas azules: punteadas.
+        // Lineas rojas: continuas.
+        dibujar_linea(display, &mut target, (86, 36), (86, 279), (-50, 0), &programa);
+        dibujar_linea(display, &mut target, (188, 93), (432, 93), (0, 50), &programa);
+        dibujar_linea(display, &mut target, (630, 58), (458, 230), (26, 26), &programa);
+        dibujar_linea(display, &mut target, (664, 50), (836, 221), (26, -26), &programa);
 
         target.finish().unwrap();
     }
